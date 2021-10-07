105120d276af09c1a23abfafd7367833742bb453
Mugeshprasath <mugeshprasath7871@gmail.com>
2021-10-07 08:25:34 -0700

    After

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,39 @@
+package org.add;
+
+public class GreensTech {
+	
+	private void greensOmr() {
+		
+		System.out.println("This is Greens tech OMR");
+
+	}
+	private void greensAdayar() {
+		System.out.println("This is Grenns tech Adayar");
+
+	}
+	private void greensTambaram() {
+		System.out.println("This is Grenns tech Tambaram");
+
+	}
+	private void greensvelachery() {
+	System.out.println("This is Greens tech velachery");
+
+	}
+	private void greensAnnanagar() {
+		System.out.println("This is Greens tech Anna nagar");
+
+	}
+	public static void main(String[] args) {
+		
+		 GreensTech greens =new  GreensTech ();
+		 
+		 greens.greensAdayar();
+		 greens.greensAnnanagar();
+		 greens.greensOmr();
+		 greens.greensTambaram();
+		 greens.greensTambaram();
+		 greens.greensvelachery();
+		
+	}
+
+}
